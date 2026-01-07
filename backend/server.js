943317fc8e7ba9e056f9e82df513b640b6612430
Noor Alam <nooralam192001@gmail.com>
2026-01-07 17:38:29 +0530

    install jwt , cookie-parser

--- backend/server.js
+++ backend/server.js
@@ -1,10 +1,12 @@
 import express from 'express'
 import todoRoute from './route/v1/todo.js';
+import cookieParser  from 'cookie-parser';
 
 
 const app = express();
 
 app.use(express.json())
+app.use(cookieParser())
 
 app.use("/api/v1/todos" , todoRoute)
 
